18174050ed423eea1ebeea3d53e9579c8ad28486
ruth melissa <ruthy9403@gmail.com>
2020-06-17 09:05:22 -0500

    ultimos detalles

--- scripts/from-num-to-letter.js
+++ scripts/from-num-to-letter.js
@@ -1,10 +1,15 @@
 let num="0";
 function myNumALetra(){
     num=prompt('Por favor ingresa un numero del 1 al 10, para ver su equivalente en letras: ');
-    let i=0;
-    while (i<= parseInt(num)) {
-        letra=String.fromCharCode(64+i);
-        ++i;
+    if(isNaN(num) || num === null){
+        alert(`Error, no digitaste un número, vuelve a ingresar`);
+        return;
+    }else{
+        let i=0;
+        while (i<= parseInt(num)) {
+            letra=String.fromCharCode(64+i);
+            ++i;
+        }
     }
     alert(`El numero ${num} equivale a la letra ${letra}`);
     document.getElementById("DemoNumALetra").innerHTML =`El numero ${num} equivale a la letra ${letra}`;
